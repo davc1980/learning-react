88afcd96aa2236df3da42201218993675c9fbe8b
davc1980 <davc1980@gmail.com>
2023-09-07 10:50:12 -0700

    simulating production plant dashboard

--- src/Components.js
+++ src/Components.js
@@ -1,5 +1,20 @@
+import { useState } from "react";
+
 export function Message({ name }) {
-  return <h1 id="msg">Hola {name}</h1>;
+  const [msg, setMsg] = useState("hola");
+
+  return (
+    <>
+      <h1 id="msg"> hola{msg}</h1>
+      <button
+        onDoubleClick={() => {
+          setMsg(name);
+        }}
+      >
+        Saludo
+      </button>
+    </>
+  );
 }
 
 export function Boton({ name }) {
@@ -21,8 +36,9 @@ export function Boton({ name }) {
 export function Card({ name, role }) {
   return (
     <>
-      <div className="card">
-        <img src="logo192.png" alt="Avatar" width={"30%"}></img>
+      <div className="card ">
+        <img src="logo192.png" alt="Avatar" width={"10%"}></img>
+        <Counter />
         <div className="container">
           <h4>
             <b>{name}</b>
@@ -31,6 +47,37 @@ export function Card({ name, role }) {
           <Boton name="hola" />
         </div>
       </div>
+    </>
+  );
+}
+
+export function Counter() {
+  const [counter, setCounter] = useState(10);
+
+  return (
+    <>
+      <h1> Counter: {counter}</h1>
+      <button
+        onClick={() => {
+          setCounter(counter + 10);
+        }}
+      >
+        +
+      </button>
+      <button
+        onClick={() => {
+          setCounter(counter - 10);
+        }}
+      >
+        -
+      </button>
+      <button
+        onClick={() => {
+          setCounter(10);
+        }}
+      >
+        Reset
+      </button>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Message, Boton, Card } from "./Components";
+import { Message, Boton, Card, Counter } from "./Components";
 import "./main.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <>
-    <Message name="Damian" />
-    {/* <Boton name="save" info="guardaste un dato" />{" "}
-    <Boton name="send" info="acabas de hacer un click" /> */}
-    <Card name="Damian Valle" role="Ingeniero IIOT" />
-    <Card name="Peregina Lares" role="Ingeniero Industrial" />
-    <Card name="Paulita Valle" role="Estudiante" />
-    <Card name="Paulita Valle" role="Estudiante" />
-    <Boton />
+    <h1 style={{ textAlign: "center", color: "blue", fontFamily: "Arial" }}>
+      Produccion Planta ACME
+    </h1>
+    <div className="flex-container">
+      <Card name="Linea 1" role="Area Moldeo" />
+      <Card name="Linea Cocedores" role="Horno" />
+      <Card name="Linea Empaque" role="Area Embarques" />
+    </div>
   </>
 );
